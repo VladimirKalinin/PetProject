38622c5903173fec15b8653bfe63f5961cf7a8c4
testKek <www.zangetsu4@gmail.com>
2016-07-18 16:40:18 +0300

    test 2

--- Test1/Test1/GraphObject.h
+++ Test1/Test1/GraphObject.h
@@ -1,35 +1,91 @@
-
+#include <glut.h>
 
 class GraphObject {
-private:
-  int X, Y, vX, vY, W, H;
+	private:
+		int X, Y, vX, vY, W, H;
 
-public:
-  GraphObject(int x, int y, int w, int h) {
-    X = x;
-    Y = y;
-    W = w;
-    H = h;
-    vX = 0;
-    vY = 0;
+	public:
+		GraphObject(int x, int y, int w, int h) {
+			X = x;
+			Y = y;
+			W = w;
+			H = h;
+			vX = 0;
+			vY = 0;
     
-  }
-  void setVx(int i){
-    vX=i;
-  }
+		}
+		int getX(){
+			return X;
+		}
+		int getY(){
+			return Y;
+		}
+		void setX(int i){
+			X=i;
+		}
+		void setY(int i){
+			Y=i;
+		}
 
-  void setVy(int i){
-    vY=i;
-  }
-  int getVx(){
-    return vX;
-  }
+		void setVx(int i){
+			vX=i;
+		}
 
-  int getVy(){
-    return vY;
-  }
-  void draw() {
-    
-  }
+		void setVy(int i){
+			vY=i;
+		}
+		int getVx(){
+			return vX;
+		}
+
+		int getVy(){
+			return vY;
+		}
+		int getW(){
+			return W;
+		}
+		int getH(){
+			return H;
+		}
+		bool cross(GraphObject go){
+			return(
+				(
+					(
+						( X>=go.getX() && X<=go.getX() + go.getW())||( X+W>=go.getX() && getX()+getW()<=go.getX()+go.getW()  )
+					) && (
+						( Y>=go.getY() && Y<=go.getY() + go.getH() )||( X+H>=go.getY() && X+H<=go.getY() + go.getH() )
+						)
+				)||(
+					(
+						( go.getX()>=X && go.getX()<=X+W )||( go.getX()+go.getW()>=X && go.getX()+go.getW()<=a.x1  )
+					) && (
+						( go.getY()>=Y && go.getY()<=a.y1 )||( go.getY() + go.getH()>=Y && go.getY() + go.getH()<=a.y1 )
+					)
+				)
+				)||(
+				(
+					(
+						( X>=go.getX() && X<=go.getX()+go.getW() )||( X+W>=go.getX() && X+W<=go.getX()+go.getW()  )
+					) && (
+						( go.getY()>=Y && go.getY()<=Y+H )||( go.getY() + go.getH()>=Y && go.getY() + go.getH()<=Y+H )
+					)
+				)||(
+					(
+						( go.getX()>=X && go.getX()<=X+W )||( go.getX()+go.getW()>=X && go.getX()+go.getW()<=X+W )
+					) && (
+						( Y>=go.getY() && Y<=go.getY() + go.getH() )||( Y+H>=go.getY() && Y + H<=go.getY() + go.getH() )
+					)
+				)
+  );
+}
+		void draw() {
+			glColor3f(1.0,1.0,1.0); //Выбираем белый цвет
+			glBegin(GL_POLYGON);
+				glVertex3f(X,Y,0.0); 
+				glVertex3f(X+W,Y,0.0); 
+				glVertex3f(X+W,Y+H,0.0); 
+				glVertex3f(X,Y+H,0.0); 
+			glEnd();
+		}
 
 };
--- Test1/Test1/Source.cpp
+++ Test1/Test1/Source.cpp
@@ -25,55 +25,23 @@ glOrtho(0,windW,0,windH,-1.0,1.0);
 
 void Draw()
 {
-glClear(GL_COLOR_BUFFER_BIT);
-phys.CalculatePhysics();
-for (auto i = ObjList.begin; i != ObjList.end; i++)
-  i->paint();
+	glClear(GL_COLOR_BUFFER_BIT);
+	phys.CalculatePhysics();
+	for (list<GraphObject>::iterator i = ObjList.begin(); i != ObjList.end(); i++)
+		i->draw();
 
-
-/*
- rectX+=vX;
-	rectY+=vY;
-//Отрисовка квадрата 
-glColor3f(((float)rectX)/400,1.0,1.0); //Выбираем белый цвет
-glBegin(GL_POLYGON);
-glVertex3f(rectX,rectY,0.0); 
-glVertex3f(rectX+rectSize,rectY,0.0); 
-glVertex3f(rectX+rectSize,rectY+rectSize,0.0); 
-glVertex3f(rectX,rectY+rectSize,0.0); 
-
-
-
-glEnd();
-if((abs(rectX-foodX)<rectSize)&&(abs(rectY-foodY))<rectSize){
-	foodX=rand()%400;
-	foodY=rand()%400;
-} 
-
-glColor3f(1.0,0,0); //Выбираем белый цвет
-glBegin(GL_POLYGON);
-glVertex3f(foodX,foodY,0.0); 
-glVertex3f(foodX+rectSize,foodY,0.0); 
-glVertex3f(foodX+rectSize,foodY+rectSize,0.0); 
-glVertex3f(foodX,foodY+rectSize,0.0); 
-
-
-*/
-glEnd();
-glFlush();
-Sleep(15);
-
-glutSwapBuffers();
+	Sleep(15);
+	glutSwapBuffers();
 }
 void key (unsigned char  key,int x,int y ){
-  if(key=='w') ObjList.front().setVy(4);
+	if(key=='w') ObjList.front().setVy(4);
 	if(key=='s') ObjList.front().setVy(-4);
 	if(key=='d') ObjList.front().setVx(4);
 	if(key=='a') ObjList.front().setVy(-4);
 	
 }
 void keyUp(unsigned char  key,int x,int y ){
-	  if(key=='w') ObjList.front().setVy(0);
+	if(key=='w') ObjList.front().setVy(0);
 	if(key=='s') ObjList.front().setVy(0);
 	if(key=='d') ObjList.front().setVx(0);
 	if(key=='a') ObjList.front().setVy(0);
@@ -81,24 +49,24 @@ void keyUp(unsigned char  key,int x,int y ){
 //Войти в главный цикл
 int main(int argc, char **argv)
 {
-  ObjList.push_back(*(new GraphObject(windW/2, windH/2, 40, 40)));
-  ObjList.push_back(*(new GraphObject(windW/2, 0, 200, 40)));
+	ObjList.push_back(*(new GraphObject(windW/2, windH/2, 40, 40)));
+	ObjList.push_back(*(new GraphObject(windW/2, 0, 200, 40)));
 
 	windH=400;
 	windW=400;
 
 
-glutInit(&argc,argv);
-glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
-glutInitWindowSize(windW,windH);		//Указываем размер окна
-glutInitWindowPosition(300,100);	//Позиция окна
-glutCreateWindow("test1");		//Имя окна
-Initialize();			//Вызов функции Initialize
-glutDisplayFunc(Draw);	
-glutIdleFunc(Draw);
-glutKeyboardFunc(key);
-glutKeyboardUpFunc(keyUp);
-glutMainLoop();
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+	glutInitWindowSize(windW,windH);		//Указываем размер окна
+	glutInitWindowPosition(300,100);	//Позиция окна
+	glutCreateWindow("test2");		//Имя окна
+	Initialize();			//Вызов функции Initialize
+	glutDisplayFunc(Draw);	
+	glutIdleFunc(Draw);
+	glutKeyboardFunc(key);
+	glutKeyboardUpFunc(keyUp);
+	glutMainLoop();
  
-return 0;
+	return 0;
 }
